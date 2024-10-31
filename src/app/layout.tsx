7bcba43c1aedbf0323940c258e56defd0a74b0b4
bigcheeseh <bigcheeseh@gmail.com>
2024-10-31 18:55:24 +0200

    init ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Web3ModalProvider initialState={{ ...initialState, open: true }}>
+        <Web3ModalProvider initialState={initialState}>
           {children}
         </Web3ModalProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 "use client";
 
-import { useAccount } from "wagmi";
+// import { useAccount } from "wagmi";
 
 export default function Home() {
-  const { isConnected } = useAccount();
-
+  // const { isConnected } = useAccount();
   return (
     <main className="min-h-screen px-8 py-0 pb-12 flex-1 flex flex-col items-center bg-white">
       <header className="w-full py-4 flex justify-between items-center">
-        <div className="flex items-center">
-          <div className="hidden sm:inline text-xl font-bold">reown AppKit example app</div>
-        </div>
-        <div className="flex items-center">
-          <w3m-button size="xxl" label="connect"/>
-          <p>{`isConnected: ${isConnected}`}</p>
+        <div className="flex items-center w-full h-full justify-between items-center">
+          <w3m-network-button />
+          <w3m-button size="md" label="connect" />
         </div>
       </header>
     </main>
