f35c7f01502b3b33f366d962f3d6f8d94dfdd0ba
Sergey <bigcheeseh@gmail.com>
2024-11-04 18:37:59 +0200

    reduce min button size

--- src/app/components/WalletButton.tsx
+++ src/app/components/WalletButton.tsx
@@ -18,7 +18,7 @@ export const WalletButton: React.FC<WalletButtonProps> = ({
   icon,
 }) => {
   const buttonClasses = classNames(
-    'flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors duration-200 min-w-48 capitalize ',
+    'flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors duration-200 min-w-32 capitalize ',
     {
       'bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-500': !disabled,
       'bg-gray-300 text-gray-700 cursor-not-allowed': disabled,
